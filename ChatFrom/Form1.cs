1912bfe80381093b8838bee93db21cfb3c20e1a6
KeyNoirr <key.garena@gmail.com>
2018-03-16 20:25:48 +0700

    Add project files.

---
+++ ChatFrom/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatFrom
+{
+    public partial class Frm : Form
+    {
+        Socket server, client;
+        IPEndPoint ipClient;
+        byte[] data;
+        public Frm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string text = txtMassage.Text;
+            listBChat.Items.Add("Server: " + text);
+            txtMassage.Text = "";
+            data = new byte[1024];
+            data = Encoding.ASCII.GetBytes(text);
+            client.Send(data);
+            data = new byte[1024];
+            client.Receive(data);
+            listBChat.Items.Add("Client: " + Encoding.ASCII.GetString(data));
+        }
+
+        private void Frm_Load(object sender, EventArgs e)
+        {
+            IPEndPoint ipServer = new IPEndPoint(IPAddress.Any, 9999);
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            server.Bind(ipServer);
+            server.Listen(5);
+            listBChat.Items.Add("Cho Ket Noi Tu Client...!");
+            client = server.Accept();
+            IPEndPoint clientEP = (IPEndPoint)client.RemoteEndPoint;
+            string lk = "Ket Noi Tu '" + clientEP.Address + "' Port '" + clientEP.Port + "'";
+            listBChat.Items.Add(lk);
+            data = new byte[1024];
+            client.Receive(data);
+            listBChat.Items.Add("Client: " + Encoding.ASCII.GetString(data));
+        }
+    }
+}
---
+++ ClientFrom/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientFrom
+{
+    public partial class Frm : Form
+    {
+        Socket client;
+        byte[] data;
+        IPEndPoint ipServer;
+        public Frm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            ipServer = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9999);
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                client.Connect(ipServer);
+                listBChat.Items.Add("Ket Noi Thanh Cong!");
+            }
+            catch (SocketException ex)
+            {
+                listBChat.Items.Add("Khong The Ket Noi Voi Server.");
+                listBChat.Items.Add(ex.ToString());
+                return;
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            data = new byte[1024];
+            data = Encoding.ASCII.GetBytes(txtMassage.Text);
+            listBChat.Items.Add("Client: " + txtMassage.Text);
+            txtMassage.Text = "";         
+            client.Send(data);
+            data = new byte[1024];
+            client.Receive(data);
+            txtMassage.Text = Encoding.ASCII.GetString(data);
+            listBChat.Items.Add("Server: " + txtMassage.Text);
+        }
+    }
+}
